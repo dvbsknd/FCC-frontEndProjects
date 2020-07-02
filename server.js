4d16ef015b8646762e20104875b81ba33d94a426
Dave Baskind <dave@picketstudio.com>
2020-07-03 08:20:01 +1000

    Set up a very basic HTTP server

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log('Running');
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.end('Hey.', console.log('Responded to', req.url));
+});
+server.listen(3000, console.log('Server is up at 3000'));
