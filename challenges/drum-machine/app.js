ddb4debbfa4e3bf5498dbc2ca9fa5e88d21b5565
Dave Baskind <dave@picketstudio.com>
2020-07-06 09:09:12 +1000

    Set up structure and state
    The drum machine now has the concept of clips, banks and pads
    and the main component stores the state of the bank of buttons,
    ie. which buttons are associated with which clips and whether
    the button is active or not. This may or may not be used in the
    actual implementation. It's not yet clear whether the machine
    will be used for sequencing or whether it just needs to play a sound
    when you click a particular button.

---
+++ challenges/drum-machine/app.js
@@ -0,0 +1,43 @@
+const CLIPS = [
+  { key: 'Q', file: 'q.mp4'}, 
+  { key: 'W', file: ''}, 
+  { key: 'E', file: ''}, 
+  { key: 'A', file: ''}, 
+  { key: 'S', file: ''}, 
+  { key: 'D', file: ''}, 
+  { key: 'Z', file: ''}, 
+  { key: 'X', file: ''}, 
+  { key: 'C', file: ''}
+];
+
+class Machine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      banks: [
+        { id: 0,
+          label: 'Main',
+          pads: CLIPS.map((clip) => ({ active: false, clipKey: clip.key, file: clip.file}))
+        }
+      ]
+    };
+  }
+  render() {
+    return(<Bank data={this.state.banks[0]} />);
+  }
+}
+
+class Bank extends React.Component {
+  render() {
+    return(
+      <ol>{this.props.data.pads.map((pad, idx) => <Pad key={idx} clip={pad} />)}</ol>);
+  }
+}
+
+class Pad extends React.Component {
+  render() {
+    return(<li><a href={this.props.clip.file}>{this.props.clip.clipKey}</a></li>);
+  }
+}
+
+ReactDOM.render(<Machine />, document.querySelector('#app'));
