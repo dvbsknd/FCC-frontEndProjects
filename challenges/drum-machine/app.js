d455022f12490f6ec2bdd842d7da0af4517d88a1
Dave Baskind <dave@picketstudio.com>
2020-07-09 08:26:09 +1000

    Make drums play with clicks and key presses
    Actual audio files are added and they can be played by clicking on the
    pads or by pressing the corresponding keys on the keyboard. Handling
    onKeyDown events was a bit tricky and it feels like this hasn't been
    done in a "React" way.

--- challenges/drum-machine/app.js
+++ challenges/drum-machine/app.js
@@ -1,13 +1,13 @@
 const CLIPS = [
-  { key: 'Q', file: 'q.mp4'}, 
-  { key: 'W', file: ''}, 
-  { key: 'E', file: ''}, 
-  { key: 'A', file: ''}, 
-  { key: 'S', file: ''}, 
-  { key: 'D', file: ''}, 
-  { key: 'Z', file: ''}, 
-  { key: 'X', file: ''}, 
-  { key: 'C', file: ''}
+  { key: 'Q', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'}, 
+  { key: 'W', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'}, 
+  { key: 'E', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'}, 
+  { key: 'A', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'}, 
+  { key: 'S', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'}, 
+  { key: 'D', file: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'}, 
+  { key: 'Z', file: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'}, 
+  { key: 'X', file: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'}, 
+  { key: 'C', file: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'}
 ];
 
 class Machine extends React.Component {
@@ -35,14 +35,32 @@ class Bank extends React.Component {
 }
 
 class Pad extends React.Component {
+  handlePadClick(e) {
+    e.preventDefault();
+    let audio = e.target.children[0];
+    audio.play();
+  }
+  handleKeyPress(e) {
+    let keyPressed = e.key.toUpperCase();
+    let audio = document.getElementById(keyPressed);
+    audio ? audio.play() : console.log('No clip for', keyPressed);
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
   render() {
     return(
       <li className={this.constructor.name + ' drum-pad'} id={`pad-${this.props.clip.clipKey}`}>
-        <a href={this.props.clip.file}>
+        <a
+          href={this.props.clip.file}
+          onClick={this.handlePadClick.bind(this)}
+          onKeyDown={(e) => this.handleKeyPress(e)}
+        >
           {this.props.clip.clipKey}
-        <audio>
-          <source src={this.props.clip.name} type="audio/mpeg" />
-        </audio>
+        <audio className='clip' id={this.props.clip.clipKey} src={this.props.clip.file} type="audio/mpeg" ></audio>
         </a>
       </li>
     );
