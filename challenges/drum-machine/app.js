fe8622a60f81868369a3a927874415e837d2249c
Dave Baskind <dave@picketstudio.com>
2020-07-10 07:56:36 +1000

    Add a track name display
    Also did some cleaning up of the mark-up based on some (mental) changes
    to the planned layout. The `setTimeout` based function to clear the
    lable after a certain amount of time is a bit buggy because of the race
    condition with the user clicking new buttons faster than the display
    disappears. There's probably a better way to do this by using a "stack"
    of labels that are waiting to be displayed and clear if a new one is
    added—or something like that.

--- challenges/drum-machine/app.js
+++ challenges/drum-machine/app.js
@@ -1,12 +1,12 @@
 const CLIPS = [
-  { key: 'Q', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'}, 
-  { key: 'W', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'}, 
-  { key: 'E', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'}, 
-  { key: 'A', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'}, 
-  { key: 'S', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'}, 
-  { key: 'D', file: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'}, 
-  { key: 'Z', file: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'}, 
-  { key: 'X', file: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'}, 
+  { key: 'Q', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'},
+  { key: 'W', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'},
+  { key: 'E', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'},
+  { key: 'A', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'},
+  { key: 'S', file: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'},
+  { key: 'D', file: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'},
+  { key: 'Z', file: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'},
+  { key: 'X', file: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'},
   { key: 'C', file: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'}
 ];
 
@@ -19,18 +19,50 @@ class Machine extends React.Component {
           label: 'Main',
           pads: CLIPS.map((clip) => ({ active: false, clipKey: clip.key, file: clip.file}))
         }
-      ]
+      ],
+      active: 'None'
     };
   }
+  updateDisplay(track) {
+    this.setState({ active: track });
+    //setTimeout(() => this.setState({ active: 'Cleared' }), 2000);
+  }
   render() {
-    return(<Bank data={this.state.banks[0]} />);
+    return(
+      <div>
+        <Bank data={this.state.banks[0]} updater={this.updateDisplay.bind(this)} />
+        <Display clipKey={this.state.active} />
+      </div>
+    );
+  }
+}
+
+class Display extends React.Component {
+  render() {
+    const clip = CLIPS.filter(clip => clip.key === this.props.clipKey)[0];
+    const file = clip ? clip.file : '';
+    const label = file
+      ? file.match(/\/drums\/(.+).mp3$/)[1].replace(/[-_]/g, ' ').toUpperCase()
+      : '';
+    console.log(this.props.clipKey, label);
+    return(<div className={this.constructor.name} id="display">{label}</div>);
   }
 }
 
 class Bank extends React.Component {
+  constructor(props){
+    super(props);
+  }
   render() {
     return(
-      <ol id="display" className={this.constructor.name}>{this.props.data.pads.map((pad, idx) => <Pad key={idx} clip={pad} />)}</ol>);
+      <ol id="drum-bank" className={this.constructor.name}>
+        {this.props.data.pads.map((pad, idx) => <Pad
+          key={idx}
+          clip={pad}
+          updater={this.props.updater}
+        />)}
+      </ol>
+    );
   }
 }
 
@@ -53,24 +85,21 @@ class Pad extends React.Component {
   handlePadClick(e) {
     e.preventDefault();
     let audio = e.target.children[0];
+    this.props.updater(this.props.clip.clipKey);
     this.playClip(audio);
   }
   handleKeyPress(e) {
     if (e.key.toUpperCase() === this.props.clip.clipKey) {
       let audio = document.getElementById(this.props.clip.clipKey);
+      this.props.updater(this.props.clip.clipKey);
       this.playClip(audio);
     }
   }
   render() {
     return(
-      <li className={this.constructor.name + ' drum-pad'} id={`pad-${this.props.clip.clipKey}`}>
-        <a
-          href={this.props.clip.file}
-          onClick={this.handlePadClick}
-        >
+      <li onClick={this.handlePadClick} className={this.constructor.name + ' drum-pad'} id={`pad-${this.props.clip.clipKey}`}>
           {this.props.clip.clipKey}
-        <audio className='clip' id={this.props.clip.clipKey} src={this.props.clip.file} type="audio/mpeg" ></audio>
-        </a>
+          <audio className='clip' id={this.props.clip.clipKey} src={this.props.clip.file} type="audio/mpeg"></audio>
       </li>
     );
   }
