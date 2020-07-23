ee17d1615bb700c02b7b0f715e8d7de9bfad6070
Dave Baskind <dave@picketstudio.com>
2020-07-24 08:10:27 +1000

    Implement toggling between Session and Break
    The clock now switches to a "Break" once the timing "Session" is over
    and starts counting down from whatever the break length is defined as.
    It changes a (new) label above the time to indicate what "mode" it is
    in, which required moving the previous concept of "mode" to "state".
    "State" now refers to whether the clock is running, stopped, etc. and
    "mode" refers to whether it's in a Session or a Break.
    
    Some minor tweaks to the settings input fields meant that we can now
    either type in values or use the increment/decrement controls to adjust
    the time settings.

--- challenges/pomodoro-clock/app.js
+++ challenges/pomodoro-clock/app.js
@@ -23,73 +23,109 @@ function formatTime(seconds, format) {
   }
 }
 
+function titleCase(str) {
+  /* Converts a string to Title Case */
+  return str.toLowerCase()
+    .split('')
+    .map((char, i) => i === 0 ? char.toUpperCase() : char)
+    .join('');
+}
+
 function Clock(props) {
-  /* The clock has a number of modes. They're states, but
-   * we call them modes so as not to confuse with application
-   * state */
-  const MODE = {
+  /* The clock has a number of states, which describe whether it's
+   * running, paused, ready, etc. Unfortunately the nomenclature is
+   * a little confusing as it ovelaps with React's "state" concept,
+   * but they are not related. */
+  const STATE = {
     ready: 'ready',
     running: 'running',
     stopped: 'stopped',
     finished: 'finished'
   }
 
-  /* The default mode is 'ready' */
-  const [mode, changeMode] = React.useState(MODE.ready);
+  /* The clock also has two modes, "Session" and "Break". Each have
+   * different time periods they can run for and they effectively 
+   * alternate indefinitely */
+  const MODE = {
+    session: 'session',
+    break: 'break'
+  }
+
+  /* The default state is 'ready' */
+  const [state, changeState] = React.useState(STATE.ready);
+
+  /* The default mode is 'session' */
+  const [mode, changeMode] = React.useState(MODE.session);
+  /* We also have a small function to flip the mode
+   * to whatever it's not. */
+  function flipMode(mode) {
+    return mode === MODE.session ? MODE.break : MODE.session;
+  }
 
   /* Default session and break lenghts are stored in state */
   const [sessionLength, changeSession] = React.useState(25 * 60);
   const [breakLength, changeBreak] = React.useState(5 * 60);
 
-  /* Set the timer */
+  /* Set the timer to the default time, ready to run. */
   const [remainingTime, setTime] = React.useState(sessionLength);
 
   /* Somewhere to store the setTimeout IDs so that we can
    * cancel them with the pause button. This allows the current
-    * loop to be immediately stopped rather than it counting
-    * down one last second via the active Timeout. */
+   * loop to be immediately stopped rather than it counting
+   * down one last second via the active Timeout. */
   let timerId;
 
   function controlClock(e) {
-    /* This click handler makes changes to the clock's state
+    /* This click handler makes changes to the clock's 'state'
      * depending on the user's selection, and logic is
      * then dealt with using React's useEffect() below
      * which is fired whenever the state changes */
     let control = e.currentTarget;
-    console.log(control);
     switch(control.id) {
       case 'start':
-        changeMode(MODE.running);
+        changeState(STATE.running);
         break;
       case 'stop':
         window.clearTimeout(timerId);
-        changeMode(MODE.stopped);
+        changeState(STATE.stopped);
         break;
       case 'reset':
         window.clearTimeout(timerId);
-        changeMode(MODE.ready);
+        changeState(STATE.ready);
         setTime(sessionLength);
         break;
       case 'session-increment':
-        if (mode === MODE.ready) {
+        if (state === STATE.ready) {
           changeSession(sessionLength + 60);
           setTime(sessionLength + 60);
         }
         break;
       case 'session-decrement':
-        if (mode === MODE.ready) {
+        if (state === STATE.ready) {
           changeSession(sessionLength - 60);
           setTime(sessionLength - 60);
         }
         break;
+      case 'session-length':
+        if (state === STATE.ready) {
+          changeSession(control.value * 60);
+          setTime(control.value * 60);
+        }
+        break;
       case 'break-increment':
-        if (mode === MODE.ready) {
+        if (state === STATE.ready) {
           changeBreak(breakLength + 60);
         }
         break;
       case 'break-decrement':
-        if (mode === MODE.ready) {
+        if (state === STATE.ready) {
           changeBreak(breakLength - 60);
+        }
+        break;
+      case 'break-length':
+        if (state === STATE.ready) {
+          changeBreak(control.value * 60);
+          setTime(control.value * 60);
         }
         break;
       default:
@@ -99,8 +135,8 @@ function Clock(props) {
 
   React.useEffect(() =>
     {
-      console.log(`${formatTime(remainingTime, 'mm:ss')} (${mode})`);
-      if (mode === MODE.running) {
+      console.log(`${formatTime(remainingTime, 'mm:ss')} (${state})`);
+      if (state === STATE.running) {
         /* In one second, reduce the remaining time by
          * one second via the supplied function, which will
          * trigger another state update and bring you back
@@ -112,8 +148,13 @@ function Clock(props) {
 
   function countDown() {
     if (remainingTime < 1) {
-      /* No more seconds on the clock, so we're finished */
-      changeMode(MODE.finished);
+      /* No more seconds on the clock, so we're finished 
+       * the current counter. If we were in a session, we
+       * switch to a break and vice versa. */
+      changeState(STATE.finished);
+      changeMode(flipMode(mode));
+      setTime(breakLength);
+      changeState(STATE.running);
     } else {
       setTime(remainingTime - 1);
     }
@@ -133,7 +174,10 @@ function Clock(props) {
 
 function Display(props) {
   return (
-    <div className='display' id='time-left'>{props.time}</div>
+    <div className='display'>
+      <h3 id='timer-label'>{titleCase(props.mode)}</h3>
+      <p id='time-left'>{props.time}</p>
+    </div>
   );
 }
 
@@ -154,6 +198,7 @@ function Controls(props) {
         <input
           type='number' id='session-length'
           name='session-length' value={props.sessionLength}
+          onChange={props.handler}
           min='0' max='60'
         />
         <button type='button' id='session-increment' onClick={props.handler}>
@@ -165,6 +210,7 @@ function Controls(props) {
         </button>
         <input type='number' id='break-length'
           name='break-length' value={props.breakLength}
+          onChange={props.handler}
           min='0' max='60'
         />
         <button type='button' id='break-increment' onClick={props.handler}>
